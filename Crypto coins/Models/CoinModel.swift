85ce91a2dadf731e2a408fbc966b34de469944a0
AlekseyFF <crain871@gmail.com>
2022-07-28 16:27:52 +0300

    Create percentage value bar

--- Crypto coins/Models/CoinModel.swift
+++ Crypto coins/Models/CoinModel.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-import Foundation
-
 // CoinGecko API info
 /*
  URL: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
@@ -52,10 +50,6 @@ import Foundation
    }
  
  */
-
-
-import Foundation
-
 
 struct CoinModel: Identifiable, Codable {
     let id, symbol, name: String
