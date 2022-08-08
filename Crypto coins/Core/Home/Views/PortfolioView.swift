b0ee20bd8546f7c88474a016f110bcf2f4964703
AlekseyFF <crain871@gmail.com>
2022-08-08 20:52:04 +0300

    Create Core Data for saving coins in Portfolio

--- Crypto coins/Core/Home/Views/PortfolioView.swift
+++ Crypto coins/Core/Home/Views/PortfolioView.swift
@@ -44,6 +44,11 @@ struct PortfolioView: View {
                     trailingNavBarButtons
                 }
             })
+            .onChange(of: vm.searchText, perform: { value in
+                if value == "" {
+                    removeSelectedCoin()
+                }
+            })
         }
     }
 }
@@ -60,13 +65,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false, content:  {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoun(coin: coin)
                             }
                         }
                         .background(RoundedRectangle(cornerRadius: 10)
@@ -76,6 +81,16 @@ extension PortfolioView {
             .frame(height: 120)
             .padding(.leading)
         })
+    }
+    
+    private func updateSelectedCoun(coin: CoinModel) {
+        selectedCoin = coin
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == coin.id}),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = "\(amount)"
+        } else {
+            quantityText = ""
+        }
     }
     
     private func getCurrentValue() -> Double {
@@ -129,10 +144,13 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else { return }
+        guard
+        let coin = selectedCoin,
+        let amount = Double(quantityText)
+        else { return }
         
         // save to portfolio
-        
+        vm.updatePortfolio(coin: coin, amount: amount)
         // show checkmark
         withAnimation(.easeIn) {
             showCheckmark = true
--- Crypto coins/Extensions/Double.swift
+++ Crypto coins/Extensions/Double.swift
@@ -5,15 +5,133 @@
 //  Created by Aleksey Fedorov on 08.07.2022.
 //
 
+//import Foundation
+//
+//extension Double {
+//    
+//    /// Converts a Double into a Currency with 2 decimal places
+//    /// ```
+//    /// Convert 1234.56 to $1,234.56
+//    /// ```
+//    private var currencyFormatter2: NumberFormatter {
+//        let formatter = NumberFormatter()
+//        formatter.usesGroupingSeparator = true
+//        formatter.numberStyle = .currency
+//        //formatter.locale = .current // <- default value
+//        //formatter.currencyCode = "usd" // <- change currency
+//        //formatter.currencySymbol = "$" // <- change currency symbol
+//        formatter.minimumFractionDigits = 2
+//        formatter.maximumFractionDigits = 2
+//        return formatter
+//    }
+//
+//    /// Converts a Double into a Currency as a String with 2 decimal places
+//    /// ```
+//    /// Convert 1234.56 to "$1,234.56"
+//    /// ```
+//    func asCurrencyWith2Decimals() -> String {
+//        let number = NSNumber(value: self)
+//        return currencyFormatter2.string(from: number) ?? "$0.00"
+//    }
+//
+//
+//    /// Converts a Double into a Currency with 2-6 decimal places
+//    /// ```
+//    /// Convert 1234.56 to $1,234.56
+//    /// Convert 12.3456 to $12.3456
+//    /// Convert 0.123456 to $0.123456
+//    /// ```
+//    private var currencyFormatter6: NumberFormatter {
+//        let formatter = NumberFormatter()
+//        formatter.usesGroupingSeparator = true
+//        formatter.numberStyle = .currency
+//        //formatter.locale = .current // <- default value
+//        //formatter.currencyCode = "usd" // <- change currency
+//        //formatter.currencySymbol = "$" // <- change currency symbol
+//        formatter.minimumFractionDigits = 2
+//        formatter.maximumFractionDigits = 6
+//        return formatter
+//    }
+//
+//    /// Converts a Double into a Currency as a String with 2-6 decimal places
+//    /// ```
+//    /// Convert 1234.56 to "$1,234.56"
+//    /// Convert 12.3456 to "$12.3456"
+//    /// Convert 0.123456 to "$0.123456"
+//    /// ```
+//    func asCurrencyWith6Decimals() -> String {
+//        let number = NSNumber(value: self)
+//        return currencyFormatter6.string(from: number) ?? "$0.00"
+//    }
+//
+//    /// Converts a Double into string representation
+//    /// ```
+//    /// Convert 1.2345 to "1.23"
+//    /// ```
+//    func asNumberString() -> String {
+//        return String(format: "%.2f", self)
+//    }
+//
+//    /// Converts a Double into string representation with percent symbol
+//    /// ```
+//    /// Convert 1.2345 to "1.23%"
+//    /// ```
+//    func asPercentString() -> String {
+//        return asNumberString() + "%"
+//    }
+//
+//
+//    /// Convert a Double to a String with K, M, Bn, Tr abbreviations.
+//    /// ```
+//    /// Convert 12 to 12.00
+//    /// Convert 1234 to 1.23K
+//    /// Convert 123456 to 123.45K
+//    /// Convert 12345678 to 12.34M
+//    /// Convert 1234567890 to 1.23Bn
+//    /// Convert 123456789012 to 123.45Bn
+//    /// Convert 12345678901234 to 12.34Tr
+//    /// ```
+//    func formattedWithAbbreviations() -> String {
+//        let num = abs(Double(self))
+//        let sign = (self < 0) ? "-" : ""
+//
+//        switch num {
+//        case 1_000_000_000_000...:
+//            let formatted = num / 1_000_000_000_000
+//            let stringFormatted = formatted.asNumberString()
+//            return "\(sign)\(stringFormatted)Tr"
+//        case 1_000_000_000...:
+//            let formatted = num / 1_000_000_000
+//            let stringFormatted = formatted.asNumberString()
+//            return "\(sign)\(stringFormatted)Bn"
+//        case 1_000_000...:
+//            let formatted = num / 1_000_000
+//            let stringFormatted = formatted.asNumberString()
+//            return "\(sign)\(stringFormatted)M"
+//        case 1_000...:
+//            let formatted = num / 1_000
+//            let stringFormatted = formatted.asNumberString()
+//            return "\(sign)\(stringFormatted)K"
+//        case 0...:
+//            return self.asNumberString()
+//
+//        default:
+//            return "\(sign)\(self)"
+//        }
+//    }
+//
+//
+//}
+
 import Foundation
 
 extension Double {
-    
+
     /// Converts a Double info Currency with 2 decimal places
     /// ```
     /// Convert 1234.56 to $1,234.56
     /// ```
-    
+
     private var currencyFormatter2: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.usesGroupingSeparator = true
@@ -25,7 +143,7 @@ extension Double {
         formatter.maximumFractionDigits = 2
         return formatter
     }
-    
+
     /// Converts a Double info Currency as with String with 2 decimal places
     /// ```
     /// Convert 1234.56 to "$1,234.56"
@@ -34,14 +152,14 @@ extension Double {
         let number = NSNumber(value: self)
         return currencyFormatter2.string(from: number) ?? "$0.00"
     }
-    
+
     /// Converts a Double info Currency with 2-6 decimal places
     /// ```
     /// Convert 1234.56 to $1,234.56
     /// Convert 12.3456 to $12.3456
     /// Convert 0.123456 to $0.123456
     /// ```
-    
+
     private var currencyFormatter6: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.usesGroupingSeparator = true
@@ -53,7 +171,7 @@ extension Double {
         formatter.maximumFractionDigits = 6
         return formatter
     }
-    
+
     /// Converts a Double info Currency as with String with 2-6 decimal places
     /// ```
     /// Convert 1234.56 to "$1,234.56"
@@ -64,7 +182,7 @@ extension Double {
         let number = NSNumber(value: self)
         return currencyFormatter6.string(from: number) ?? "$0.00"
     }
-    
+
     /// Converts a Double info string representation
     /// ```
     /// Convert 1.2345 to "1.23"
@@ -73,7 +191,7 @@ extension Double {
     func asNumberString() -> String {
         return String(format: "%.2f", self)
     }
-    
+
     /// Converts a Double info string representation with percent symbol
     /// ```
     /// Convert 1.2345 to "1.23%"
@@ -81,7 +199,7 @@ extension Double {
     func asPercentString() -> String {
         return asNumberString() + "%"
     }
-    
+
     /// Convert a Double to a String with K, M, Bn, Tr abbreviations.
     /// ```
     /// Convert 12 to 12.00
